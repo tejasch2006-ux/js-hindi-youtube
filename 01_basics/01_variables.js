7ee4ccece864f2990a3a66e12b478b6c8e68e2d4
Tejas <tejasch2006@gmail.com>
2025-11-26 20:27:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "tejasch2006@gmail.com"
+var accountPasseord = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+accountEmail = "hgsf@hc.com"
+accountPasseord ="6578585"
+accountCity = "Pune"
+
+
+/* 
+Prefer Not to use var
+because of issue in block Scope and Function Scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPasseord, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 )// We are using Node JS not Browser
+
+console.log(3 
+    + 
+    3) //Code readability should be high
+
+
+console.log("Hitesh")
+
+
+let name = "Tejas"
+let age = 18
+let isloggedIn = false
+let state = null
+
+// Number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
