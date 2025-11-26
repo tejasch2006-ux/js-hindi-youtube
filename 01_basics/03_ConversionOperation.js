066b2f90f593a63591ec8939bc8ad18dad8eed61
Tejas <tejasch2006@gmail.com>
2025-11-26 20:48:07 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Tejas"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "Tejas"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"Hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
