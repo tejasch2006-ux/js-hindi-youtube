c06ffa84489102505cc249c6088d275950d346ba
Tejas <tejasch2006@gmail.com>
2025-12-08 18:17:18 +0000

    05_strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,7 +7,12 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hiteshhc')
 
-console.log(gameName[0])
-console.log(gameName.__proto__);
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
 
-console.log(gameName,length);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+
